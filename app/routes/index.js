83a904f2eece7c73d461bc72da52cd6b988fa08f
David M <david.magee15@yahoo.com>
2017-12-14 01:27:11 +0000

    added limit of 5 stocks, added code to handle duplicate stocks

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,9 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 var List = require('../models/list.js');
-var yahooFinance = require('yahoo-finance');
 
-module.exports = function (app, passport, io) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
+module.exports = function (app, yahooFinance, io) {
 	
 	io.on('connection', function(client){
     console.log("IO CLIENT CONNECTED");
@@ -29,7 +17,8 @@ module.exports = function (app, passport, io) {
 				}
 				else{
 				yahooFinance.historical({
-					symbols: tempArray
+					symbols: tempArray,
+					from: '2012-01-01',
 					}, function (err, news) {
 					if (err){throw err;}
 					var j = news;
@@ -75,95 +64,6 @@ module.exports = function (app, passport, io) {
 				}
 				
 			});
-/*   
-    List
-				.find({}, function (err, result) {
-				if (err) { throw err; }
-				
-				var tempArray = result[0].stockList;
-				var length = tempArray.length;
-
-				for(var x=0;x<length;x++){
-					tempArray[x] = 'NASDAQ:'+tempArray[x];
-				}
-				googleFinance.historical({
-					symbols: tempArray
-					}, function (err, news) {
-					if (err){throw err;}
-					var j = news;
-					
-					var totalSeries = [];
-        if(j==null){
-            client.emit('update', null);
-        }
-        else{
-            
-        var newArray;
-        var colors = ['red','green','blue','orange','purple'];
-        var colorCount = 0;
-
-        for(var propName in j) {
-            newArray = [];
-        if(j.hasOwnProperty(propName)) {
-            var propValue = j[propName];
-
-        propValue.forEach(function(item) {
-            var val = [(new Date(item.date)).getTime(),item.open];
-			newArray.push(val);
-        });
-        
-        newArray = newArray.sort(function(a, b) {
-        return a[0] - b[0]; });
-        var newSeries = {
-		        name: propName,
-		        data: newArray,
-		        color: colors[colorCount]
-		        };
-		 colorCount++;
-		 totalSeries.push(newSeries);
-
-        // do something with each element here
-         }
-        }
-        
-        client.emit('update', totalSeries);
-                
-            }
-				});
-					
-				
-			});
-*/			
-/*	client.on('update', function(){
-		List
-				.find({}, function (err, result) {
-				if (err) { throw err; }
-
-				console.log("retrieved data: "+result[0].stockList);
-				console.log(result[0].stockList);
-				
-				var tempArray = result[0].stockList;
-				console.log(typeof tempArray);
-				var length = tempArray.length;
-				console.log(length);
-				
-				for(var x=0;x<length;x++){
-					tempArray[x] = 'NASDAQ:'+tempArray[x];
-				}
-				googleFinance.historical({
-					symbols: tempArray
-					}, function (err, news) {
-					if (err){throw err;}
-					var sentArray = news;
-					io.sockets.emit('update', sentArray);
-					});
-					
-				
-			});
-	}); */
-    
-    
-    
     
 	client.on('delete', function(data){
 
@@ -183,7 +83,8 @@ module.exports = function (app, passport, io) {
 				var tempArray = result[0].stockList;
 				
 				yahooFinance.historical({
-					symbols: tempArray
+					symbols: tempArray,
+					from: '2012-01-01',
 					}, function (err, news) {
 					if (err){throw err;}
 					var j = news;
@@ -218,7 +119,6 @@ module.exports = function (app, passport, io) {
 		 colorCount++;
 		 totalSeries.push(newSeries);
 
-        // do something with each element here
          }
         }
         
@@ -235,18 +135,18 @@ module.exports = function (app, passport, io) {
 	});
 	
 	client.on('add', function(data){
-        // this can currently only be done server-side.
-        // data is now available to be searched inside or outside of this function.
+
         List
 			.find({}, function (err, result) {
 				if (err) { throw err; }
 
-				if(!result[0].stockList.includes(data)){
+				if(!result[0].stockList.includes(data) && result[0].stockList.length<5){
 					
 					var tempArray = result[0].stockList;
 					tempArray.push(data.toUpperCase());
 					yahooFinance.historical({
-					symbols: tempArray
+					symbols: tempArray,
+					from: '2012-01-01',
 					}, function (err, news) {
 					if (err){throw err;}
 					var j = news;
@@ -280,7 +180,6 @@ module.exports = function (app, passport, io) {
 		 colorCount++;
 		 totalSeries.push(newSeries);
 
-        // do something with each element here
          }
         }
         var k = totalSeries.filter(removeEmpty);
@@ -301,7 +200,51 @@ module.exports = function (app, passport, io) {
 					
 					
 				}
-					
+				else{
+					var tempArray = result[0].stockList;
+					yahooFinance.historical({
+					symbols: tempArray,
+					from: '2012-01-01',
+					}, function (err, news) {
+					if (err){throw err;}
+					var j = news;
+					var totalSeries = [];
+        if(j==null){
+            io.sockets.emit('update', null);
+        }
+        else{
+            
+        var newArray;
+        var colors = ['red','green','blue','orange','purple','yellow','black'];
+        var colorCount = 0;
+
+        for(var propName in j) {
+            newArray = [];
+        if(j.hasOwnProperty(propName)) {
+            var propValue = j[propName];
+
+        propValue.forEach(function(item) {
+            var val = [(new Date(item.date)).getTime(),item.open];
+			newArray.push(val);
+        });
+        
+        newArray = newArray.sort(function(a, b) {
+        return a[0] - b[0]; });
+        var newSeries = {
+		        name: propName,
+		        data: newArray,
+		        color: colors[colorCount]
+		        };
+		 colorCount++;
+		 totalSeries.push(newSeries);
+
+         }
+        }
+        io.sockets.emit('update', totalSeries);
+
+            }
+				});
+				}	
 			
 			});
 		
@@ -318,142 +261,6 @@ module.exports = function (app, passport, io) {
 		.get(function (req, res) {
 			res.sendFile(path + '/dev/index.html');
 		});
-	
-	app.route('/retrievedata/')
-		.get(function (req, res){
-			
-			List
-			.find({}, function (err, result) {
-				if (err) { throw err; }
-				
-				if(result[0].stockList.length==0){
-					var firstList = new List({
-		    				'stockList': []
-							});
-							firstList.save();
-							res.send(null);
-				}
-				else{
-					
-				
-				var tempArray = result[0].stockList;
-				var length = tempArray.length;
 
-				for(var x=0;x<length;x++){
-					tempArray[x] = 'NASDAQ:'+tempArray[x];
-				}
-				googleFinance.historical({
-					symbols: tempArray
-					}, function (err, news) {
-					if (err){throw err;}
-					var sentArray = news;
-					res.send(sentArray);
-					});
-					
-				}	
-			});
-			
-		});
-		
-	app.route('/retrievedata/:id')
-		.get(function (req, res){
-			
-			
-			List
-			.find({}, function (err, result) {
-				if (err) { throw err; }
 
-				console.log("retrieved data: "+result[0].stockList);
-				
-				if(!result[0].stockList.includes(req.params.id)){
-					List
-					.findOneAndUpdate({}, {$push: {"stockList": req.params.id}}, function(err, results2){
-						if(err){throw err;}
-						
-						List.find({}, function(err, results3){
-							if(err)throw err;
-							
-							var tempArray = results3[0].stockList;
-							console.log(typeof tempArray);
-							var length = tempArray.length;
-							console.log(length);
-				
-							for(var x=0;x<length;x++){
-								tempArray[x] = 'NASDAQ:'+tempArray[x];
-							}
-							googleFinance.historical({
-							symbols: tempArray
-							}, function (err, news) {
-							if (err){throw err;}
-							var sentArray = news;
-							res.send(sentArray);
-							});
-							});
-						
-					});
-					
-				}
-					
-			
-			});
-		});
-		
-	app.route('/deletedata')
-		.post(function (req,res){
-			var regex = /[A-Z]{2,5}$/;
-			var entry = req.body.data.match(regex);
-			console.log("Post request");
-			console.log("REGEX: "+entry);
-				
-				List
-					.update({}, {$pull: {'stockList': String(entry)}}, function(err, results2){
-						if(err){throw err;}
-					console.log(results2);
-					
-					List.find({}, function(err, results3){
-							if(err)throw err;
-							
-							console.log("Final delete results: "+results3[0].stockList);
-							});
-					
-					
-					});
-				res.end();
-			
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/google')
-		.get(passport.authenticate('google', { scope: ['profile'] }));
-
-	app.route('/oauth2callback')
-		.get(passport.authenticate('google', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
--- server.js
+++ server.js
@@ -4,14 +4,13 @@
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
-var passport = require('passport');
 var session = require('express-session');
 var bodyparser = require('body-parser');
 var path = require('path');
+var yahooFinance = require('yahoo-finance');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
@@ -23,20 +22,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyparser.urlencoded({'extended': true}));
 app.use(bodyparser.json())
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/dev', express.static(process.cwd() + '/dev'));
 app.use('/output', express.static(process.cwd() + '/output'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
 	secret: 'secretClementine',
 	resave: false,
 	saveUninitialized: true
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -45,7 +39,7 @@ app.use(function(req, res, next) {
 });
 
 
-routes(app, passport, io);
+routes(app, yahooFinance, io);
 
 var port = process.env.PORT || 8080;
 http.listen(port,  function () {
